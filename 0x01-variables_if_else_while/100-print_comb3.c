b3ec234f5f2da25cf03ae01705b6b6b254f66c7d
naomimwangi <naomimwangi7@gmail.com>
2022-06-05 08:29:40 -0700

    add 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,47 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main - main block
- * Description: Write a program that prints all possible
- * different combinations of two digits
- * Numbers must be separated by ,, followed by a space
- * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Print only the smallest combination of two digits
- * Numbers should be printed in ascending order, with two digits
- * You can only use the putchar function
- * (every other function (printf, puts, etc…) is forbidden)
- * You can only use putchar five times maximum in your code
- * You are not allowed to use any variable of type char
- * All your code should be in the main function
- * Return: 0
+ * main -> assign a random number to the variable n each time it is executed
+ * and print the last digit of the number stored in the variable n
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-		int c;
-			int d = 0;
+	int ch;
+	int n;
+	for (ch = 48; ch <= 57; ch++)
+	{
+		for (n = 49; n <= 57; n++)
+		{
+			if (n > ch)
+			{
+				putchar(ch);
+				putchar(n);
+				if (ch != 56 || n != 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
 
-				while (d < 10)
-						{
-									c = 0;
-											while (c < 10)
-														{
-																		if (d != c && d < c)
-																						{
-																											putchar('0' + d);
-																															putchar('0' + c);
+	putchar(10); /* this is an ascii code for new line*/
 
-																																			if (c + d != 17)
-																																								{
-																																														putchar(',');
-																																																			putchar(' ');
-																																																							}
-																																						}
-
-																					c++;
-																							}
-													d++;
-														}
-					putchar('\n');
-						return (0);
+	return (0);
 }
