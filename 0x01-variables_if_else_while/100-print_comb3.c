35a30ee95f454965366f5e537ed0e1d2d48475d3
naomimwangi <naomimwangi7@gmail.com>
2022-06-05 07:22:02 -0700

    add 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,6 +9,8 @@ int main(void)
 {
 	int ch;
 	int n;
+
+
 	for (ch = 48; ch <= 57; ch++)
 	{
 		for (n = 49; n <= 57; n++)
@@ -17,16 +19,22 @@ int main(void)
 			{
 				putchar(ch);
 				putchar(n);
+
 				if (ch != 56 || n != 57)
 				{
 					putchar(44);
 					putchar(32);
 				}
+				
 			}
 		}
+
+		
 	}
 
+
 	putchar(10); /* this is an ascii code for new line*/
+
 
 	return (0);
 }
