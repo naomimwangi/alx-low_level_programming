48e8fbe0a80786c6b744ca0c6a6b565af1ff34c0
naomimwangi <naomimwangi7@gmail.com>
2022-06-05 08:33:06 -0700

    add 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,6 +5,7 @@
  * and print the last digit of the number stored in the variable n
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int ch;
@@ -17,6 +18,7 @@ int main(void)
 			{
 				putchar(ch);
 				putchar(n);
+
 				if (ch != 56 || n != 57)
 				{
 					putchar(44);
