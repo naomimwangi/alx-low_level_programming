56dc852f4643159f62e22dcc6d7cd4cc16c52484
naomimwangi <naomimwangi7@gmail.com>
2022-06-05 04:48:00 -0700

    add 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 /**
  *  * main - Entry point
- *   * Return:1
- *    */
+ *  * Return:1
+**/
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
