86dce985bb211dfbdda49956cdab5ef6bc09221b
naomimwangi <naomimwangi7@gmail.com>
2022-06-08 05:23:44 -0700

    add 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,16 +8,17 @@ int main(void)
 {
 	int i, z = 0;
 
-        	while (i < 1024)
+        	
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-		        	if ((i % 3 == 0) || (i % 5 == 0))
-				{
-					z += i;
-				}
-
-					i++;
+		        z += i;
 		}
 
-				printf("%d\n", z);
-				return (0);
+		i++;
+	}
+
+	printf("%d\n", z);
+	return (0);
 }
