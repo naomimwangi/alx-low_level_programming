c8034240a83ff833eaee1fd3b5a719361db2f8e5
naomimwangi <naomimwangi7@gmail.com>
2022-06-09 13:48:35 -0700

    correct 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,30 +1,24 @@
 #include "main.h"
 /**
- * print_diagonal - prints diagonal line
+ * print_line - draws a straight line in the terminal.
  * @n: print int
  * Return: Always 0.
  */
-void print_diagonal(int n)
+void print_line(int n)
 {
 
+
 	int i;
-	int b;
 
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (b = 0; b < i; b++)
-			{
-				_putchar (' ');
-			}
-			_putchar (92);
-			_putchar ('\n');
-
+			_putchar (95);
 		}
 
+		_putchar ('\n');
 	}
-
 
 	else
 	{
