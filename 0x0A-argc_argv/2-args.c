12e25926a9a9c350a4641d2640260f8b4a470ca0
naomimwangi <naomimwangi7@gmail.com>
2022-06-24 23:37:28 -0700

    add 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Argument
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+
